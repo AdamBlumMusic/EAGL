c45e2dc5a207787032bd033962d4d9f32b0b6c05
Adam Blum <adamblum@adamblummusic.com>
2013-08-24 19:46:17 -0400

    Making my code purtyyy

--- includes/functions.php
+++ includes/functions.php
@@ -22,19 +22,11 @@ function eagl_getFilesize($file,$digits = 2) {
 }
 
 
-$eagl_scriptsarray = explode(", ",$eagl_scripts);
-reset($eagl_scriptsarray);
-
+    $eagl_scriptsarray = explode(", ",$eagl_scripts);
+    reset($eagl_scriptsarray);
 		while (list(, $value) = each($eagl_scriptsarray)) {
 		    echo "\t<script src='scripts/$value.js'></script> <!-- $value -->\n";
 		}
-
-
-
-
-
-
-
 
 $eagl_stylesarray = explode(", ",$eagl_stylesheets);
 reset($eagl_stylesarray);
@@ -43,14 +35,8 @@ reset($eagl_stylesarray);
     echo "\t<link rel='stylesheet' href='style/css/$eagl_stylesheet.css' /> <!-- $eagl_stylesheet -->\n";
   }
 
-
-
-
 echo "\t<!--[if lte IE 6]>\n\t\t<link rel='stylesheet' href='style/css/$eagl_legacystylesheet.css' />\n\t<![endif]-->\n";
-
 echo "<style> nav a[href='$eagl_thispage'] {color:  #eb5448;} </style>\n";
-
-
 
 function eagl_createSlug($string){
   $slug = preg_replace('/[^A-Za-z0-9-]+/', '-', $string);
@@ -62,11 +48,9 @@ function eagl_createSlug($string){
 
 
 
-      //date in mm/dd/yyyy format; or it can be in other formats as well
-      //explode the date to get month, day and year
-      $birthDate = explode("/", $birthDate);
-      //get age from date or birthdate
-      $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md") ? ((date("Y")-$birthDate[2])-1):(date("Y")-$birthDate[2]));
+$birthDate = explode("/", $birthDate);
+//get age from date or birthdate
+$age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md") ? ((date("Y")-$birthDate[2])-1):(date("Y")-$birthDate[2]));
 
 
 
@@ -75,25 +59,18 @@ function ago($time)
 {
    $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
    $lengths = array("60","60","24","7","4.35","12","10");
-
    $now = time();
-
-       $difference     = $now - $time;
-       $tense         = "ago";
-
+       $difference = $now - $time;
+       $tense = "ago";
    for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
        $difference /= $lengths[$j];
    }
-
    $difference = round($difference);
-
    if($difference != 1) {
        $periods[$j].= "s";
    }
-
    return "$difference $periods[$j] ago ";
 }
-
 
 
 function eagl_generateBloglist() {
@@ -115,31 +92,19 @@ function eagl_generateBlogpost() {
   echo file_get_contents("blog/$post");
 }
 
-
-
-
 function eagl_includeAll($dir) {
   foreach (glob("$dir/*.php") as $filename) {
     include $filename;
   }
 }
 
-
-
 function redirect($location) {
   header( "Location: $location" ) ;
 }
 
-
-
-
-
 function eagl_alert($title, $message) {
   echo "<div role='alert'><h4>$title</h4><p>$message</p></div>";
 }
-
-
-
 
 $eagl_user = "adamblum";
 
@@ -149,15 +114,10 @@ $eagl_today = date("F j Y");
 
 $eagl_todayNum = date("Y n d");
 
-
-
 function eagl_login() {
 
 
 }
-
-
-
 
 function eagl_secure() {
   if ($_SESSION['loggedIn'] == "true") {
@@ -166,8 +126,6 @@ function eagl_secure() {
     header( "Location: login.php" ) ;
   }
 }
-
-
 
 function eagl_loginSuccess() {
   $_SESSION['loggedIn'] = "true";
@@ -182,20 +140,10 @@ function eagl_loginFailure() {
   echo $passAttemptNoHash;
 }
 
-
-
 if ($eagl_scripts !== "" && $eagl_stylesheets !== "" && $eagl_legacystylesheet !== "" && $eagl_nostyles == "false" && $eagl_developerInfo == "false") {
   $eagl_productionReady = "true";
 } else {
   $eagl_productionReady = "false";
 }
-
-
-
-
-
-
-
-
 
 ?>
