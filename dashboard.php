d23736a90d411dfb8f09b5bd5e76047a45155fef
Adam Blum <adamblum@adamblummusic.com>
2013-08-24 19:40:21 -0400

    Fixing Indentation

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <?php include "includes/head.php"; ?>
-	<title>Adam Blum</title>
+<title>Adam Blum</title>
 </head>
 <body>
 	<?php include "includes/legacy.php"; ?>
@@ -7,11 +7,6 @@
 	<?php eagl_secure(); ?>
 <h3>Welcome, <?php echo $eagl_admin; ?>!</h3>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eius odit culpa fuga quos illo corporis quisquam? Officia, reiciendis molestias enim officiis quaerat maiores aut ut numquam incidunt rerum! Tempore.</p>
-
-<article>
-
-</article>
-
 	<?php include "includes/footer.php"; ?>
 </body>
 </html>
