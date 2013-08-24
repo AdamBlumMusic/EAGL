d9dbd7d52367ddb82b70444c83a499c599e43d85
Adam Blum <adamblum@adamblummusic.com>
2013-08-24 19:20:53 -0400

    Added Blank.php
    A blank file used for creating new pages. Has all the php includes and
    stuff in it.

---
+++ blank.php
@@ -0,0 +1,18 @@
+<?php include "includes/head.php"; ?>
+<title>Adam Blum</title>
+</head> 
+<body>
+<?php include "includes/legacy.php"; ?>
+<?php include "includes/navigation.php"; ?>
+
+
+<!--
+	Blank Page Template
+	Add Content Here
+-->
+
+
+
+<?php include "includes/footer.php"; ?>
+</body>
+</html>
