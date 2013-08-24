dbe8a647ccebd9a2070223fc59f7337b41d83f3d
Adam Blum <adamblum@adamblummusic.com>
2013-08-24 19:57:02 -0400

    Content

--- features.php
+++ features.php
@@ -10,7 +10,21 @@
 	<header>
 		<h1>Features</h1>
 	</header>
+
+	<h2>CSS</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, dignissimos laborum ratione quod sequi repellat repellendus eligendi dolorem perferendis quisquam est modi eveniet blanditiis molestias non? Quam, facere recusandae quod.</p>
+
+	<h3>Typography</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, iure, debitis distinctio amet dolorum libero optio officia quis animi voluptates corrupti ad sint laudantium cupiditate laboriosam aut deleniti quod repellendus.</p>
+
+
+	<h2>Javascript</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, similique, accusantium repudiandae sequi consequatur sed ab quam fugiat mollitia et sit ducimus aut alias facere rem quis exercitationem beatae aliquam.</p>
+
+
 </article>
+
+
 
 
 
