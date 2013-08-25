217a3d9dfc7fe18370b42bd5d1ccd52dcb1ee0ac
Adam Blum <adamblum@adamblummusic.com>
2013-08-24 20:19:38 -0400

    Content

--- features.php
+++ features.php
@@ -17,10 +17,8 @@
 	<h3>Typography</h3>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, iure, debitis distinctio amet dolorum libero optio officia quis animi voluptates corrupti ad sint laudantium cupiditate laboriosam aut deleniti quod repellendus.</p>
 
-
 	<h2>Javascript</h2>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, similique, accusantium repudiandae sequi consequatur sed ab quam fugiat mollitia et sit ducimus aut alias facere rem quis exercitationem beatae aliquam.</p>
-
 
 	<h2>Admin Dashboard</h2>
 	<p>The admin section features a new custom menu at the top of the website (similar to WordPress), and additional pages where you can create new blog posts, editing configuration files, and check general site statistics.</p>
